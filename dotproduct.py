cdc5a07e3fc5609bfa4d7845162da3e70fbe4f0c
Joocheol <joocheol@yonsei.ac.kr>
2023-10-27 03:29:37 +0900

    ok

--- dotproduct.py
+++ dotproduct.py
@@ -4,6 +4,6 @@ class DotProduct(VectorScene):
     def construct(self):
 
         plane = self.add_plane(animate=True)
-        a = self.add_vector([2,1])
+        a = self.add_vector([2,2])
 
         self.wait()
