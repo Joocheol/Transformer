303e04b1cc543594b35413c5e1731837fc854d90
Joocheol <joocheol@yonsei.ac.kr>
2023-10-27 13:44:34 +0900

    ok

--- dotproduct.py
+++ dotproduct.py
@@ -1,32 +1,65 @@
 from manim import *
 
 
-class DotProduct(Scene):
+class DotProduct(VectorScene):
     def construct(self):
 
-        plane = NumberPlane()
-        a = np.array([5,1,0])
-        b = np.array([2,2,0])
-        proj = a * np.dot(a,b)/np.dot(a,a)
+        # plane = NumberPlane()
+        # a = np.array([5,1,0])
+        # b = np.array([2,2,0])
+        # proj = a * np.dot(a,b)/np.dot(a,a)
 
         
 
-        aa = Vector(a)
-        bb = Vector(b)
-        theta = Angle(aa, bb, radius=1)
-        projj = Vector(proj)
-        dist1 = Line(a,b)
-        dist2 = Line(b,proj)
+        # kk = Vector(a)
+        # qq = Vector(b)
+        # theta = Angle(kk, qq, radius=1)
+        # projj = Vector(proj)
+        # dist1 = Line(a,b)
+        # dist2 = Line(b,proj)
         
 
-        self.add(plane)
-        self.play(Write(aa))
-        self.play(Write(bb))
-        self.play(Write(theta))
-        self.play(Write(projj))
-        self.play(Write(dist1))
-        self.play(Write(dist2))
+        # self.add(plane)
+        # self.play(Write(kk))
+        # self.play(Write(qq))
+        # self.play(Write(theta))
+        # self.play(Write(projj))
+        # self.play(Write(dist1))
+        # self.play(Write(dist2))
+        
+        # plane = self.add_plane()
+        # k = np.array([3,-1,0])
+        # q = np.array([2,2,0])
+        # kk = self.add_vector(k)
+        # qq = self.add_vector(q)
+        # # self.write_vector_coordinates(qq)
+        # # self.write_vector_coordinates(kk)
+        # angle = Angle(kk, qq, radius=0.5)
         
 
+        # #self.play(Write(MathTex(r"k").scale(LARGE_BUFF-0.2).next_to(kk.get_center(), DOWN)))
+        # #self.label_vector(qq, MathTex("q"))
+        # self.play(Write(angle))
+        # theta = MathTex(r"\theta").next_to(angle, RIGHT, buff=SMALL_BUFF)
+        # self.play(Write(theta))
 
-        self.wait()
+        # norm_q = np.dot(q,q)
+
+        # proj1 = k * np.dot(k,q)/np.dot(k,k)
+        # proj2 = q * np.dot(k,q)/np.dot(q,q)
+
+        # # math_1 = MathTex(r"q + k = ")
+        # # math_2 = MathTex(r"q - k = ")
+        # # math_3 = MathTex(r"\| q \| = \sqrt{2^2 + 2^2}")
+        # # math_4 = MathTex(r"\| k \| = \sqrt{3^2 + (-1)^2}")
+        # # vg = VGroup(math_1, math_2, math_3, math_4).arrange(DOWN).to_edge(buff=LARGE_BUFF)
+
+        # # self.add(vg)
+        # self.wait()
+
+        # self.play(Write(Line(q, proj1)))
+        # self.play(Write(Line(k, proj2)))
+
+
+
+
