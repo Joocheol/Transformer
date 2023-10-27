b07701733e827326abadc36bd5c4ce3a9fecd6b0
Joocheol <joocheol@yonsei.ac.kr>
2023-10-27 15:42:28 +0900

    ok

--- dotproduct.py
+++ dotproduct.py
@@ -1,65 +1,39 @@
 from manim import *
 
 
-class DotProduct(VectorScene):
+class DotProduct(Scene):
     def construct(self):
 
-        # plane = NumberPlane()
-        # a = np.array([5,1,0])
-        # b = np.array([2,2,0])
-        # proj = a * np.dot(a,b)/np.dot(a,a)
-
-        
-
-        # kk = Vector(a)
-        # qq = Vector(b)
-        # theta = Angle(kk, qq, radius=1)
-        # projj = Vector(proj)
-        # dist1 = Line(a,b)
-        # dist2 = Line(b,proj)
-        
-
+        plane = NumberPlane()
         # self.add(plane)
-        # self.play(Write(kk))
-        # self.play(Write(qq))
-        # self.play(Write(theta))
-        # self.play(Write(projj))
-        # self.play(Write(dist1))
-        # self.play(Write(dist2))
         
-        # plane = self.add_plane()
-        # k = np.array([3,-1,0])
-        # q = np.array([2,2,0])
-        # kk = self.add_vector(k)
-        # qq = self.add_vector(q)
-        # # self.write_vector_coordinates(qq)
-        # # self.write_vector_coordinates(kk)
-        # angle = Angle(kk, qq, radius=0.5)
-        
+        vt = ValueTracker(-1)
+        line_0 = Line(ORIGIN, [3, 0, 0]).set_color(YELLOW)
+        # line_1 = Line(ORIGIN, [0, 1, 0]).add_updater(lambda m: m.become(Line(ORIGIN, [-2*np.sin(vt.get_value()), 2*np.cos(vt.get_value()), 0]).set_color(BLUE)))
+        line_1 = Line(ORIGIN, [0, 1, 0]).add_updater(lambda m: self.my_fun(m, vt))
+        proj = Line(ORIGIN, line_1.get_end()).add_updater(lambda m: self.my_proj(m, line_1, vt))
+        angle = Angle(line_0, line_1).add_updater(lambda m: m.become(Angle(line_0, line_1)))
 
-        # #self.play(Write(MathTex(r"k").scale(LARGE_BUFF-0.2).next_to(kk.get_center(), DOWN)))
-        # #self.label_vector(qq, MathTex("q"))
-        # self.play(Write(angle))
-        # theta = MathTex(r"\theta").next_to(angle, RIGHT, buff=SMALL_BUFF)
-        # self.play(Write(theta))
+        number = DecimalNumber(0, color=RED).to_corner().add_updater(lambda m: self.my_num(m, line_1))
 
-        # norm_q = np.dot(q,q)
+        self.add(line_0, line_1, angle, proj, number)
+        self.play(vt.animate.set_value(10), run_time=5, rate_func=linear)
 
-        # proj1 = k * np.dot(k,q)/np.dot(k,k)
-        # proj2 = q * np.dot(k,q)/np.dot(q,q)
+    def my_fun(self, mob, vt):
 
-        # # math_1 = MathTex(r"q + k = ")
-        # # math_2 = MathTex(r"q - k = ")
-        # # math_3 = MathTex(r"\| q \| = \sqrt{2^2 + 2^2}")
-        # # math_4 = MathTex(r"\| k \| = \sqrt{3^2 + (-1)^2}")
-        # # vg = VGroup(math_1, math_2, math_3, math_4).arrange(DOWN).to_edge(buff=LARGE_BUFF)
+        return mob.become(Line(ORIGIN, [-2*np.sin(vt.get_value()), 2*np.cos(vt.get_value()), 0]).set_color(BLUE))
+    
+    def my_num(self, m, line_1):
+        a = line_1.get_end()
+        b = np.array([3,0,0])
+        tmp = a * np.dot(a,b)/np.dot(a,a)
+        tmp = b * np.dot(a,b)/np.dot(b,b)
 
-        # # self.add(vg)
-        # self.wait()
-
-        # self.play(Write(Line(q, proj1)))
-        # self.play(Write(Line(k, proj2)))
-
-
-
-
+        return m.set_value(np.dot(a,b))
+    
+    def my_proj(self, mob, line_1, vt):
+        a = line_1.get_end()
+        b = np.array([3,0,0])
+        tmp = a * np.dot(a,b)/np.dot(a,a)
+        tmp = b * np.dot(a,b)/np.dot(b,b)
+        return mob.become(Line(line_1.get_end(), tmp))
